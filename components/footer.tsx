9ef0611f40408e28bf5524d3b051895e44aa01e7
Reinhart Barus <reintbarus@gmail.com>
2025-06-17 17:48:20 +0700

    change footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="pt-8 text-center text-slate-500 text-sm bg-white">
-      &copy; 2025 BelajarEnkripsi. All rights reserved.
+      &copy; 2025 SecureLearn. All rights reserved.
     </footer>
   );
 }
